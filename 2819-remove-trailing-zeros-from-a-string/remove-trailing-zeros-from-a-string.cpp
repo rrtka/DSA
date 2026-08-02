4070c44b87a1473717e5cb409c973bd85a9b2aa7
R Ritika <ritikafyi@gmail.com>
2026-08-02 12:16:25 +0530

    Time: 4 ms (17.22%) | Memory: 11.6 MB (54.57%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.cpp
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     string removeTrailingZeros(string num) {
         int last=num.size()-1;
-        int c=0;
         while(num[last]=='0'){
-            c++;
             last--;
         }
-        string ns=num.substr(0,num.size()-c);
-        return ns;
+        return num.substr(0,last+1);
     }
 };
